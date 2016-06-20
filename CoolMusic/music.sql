99f6c0a431b6293316f1cc1c1dfa55ad80be36e8
90047872j <usu21@novaulas.stucom.com>
2016-06-20 09:18:49 +0200

    Añadida la base de datos otra vez

---
+++ CoolMusic/music.sql
@@ -0,0 +1,184 @@
+CREATE DATABASE  IF NOT EXISTS `musica` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `musica`;
+-- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: musica
+-- ------------------------------------------------------
+-- Server version	5.6.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `album`
+--
+
+DROP TABLE IF EXISTS `album`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `album` (
+  `name` varchar(40) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `album`
+--
+
+LOCK TABLES `album` WRITE;
+/*!40000 ALTER TABLE `album` DISABLE KEYS */;
+INSERT INTO `album` VALUES ('Geordie','2001-02-02 00:00:00'),('My All','1997-02-25 00:00:00'),('No Woman No Cry','1979-08-23 00:00:00'),('Titanic BSO','1999-06-15 00:00:00');
+/*!40000 ALTER TABLE `album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `artist`
+--
+
+DROP TABLE IF EXISTS `artist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `artist` (
+  `singer` varchar(40) NOT NULL,
+  `genre` varchar(20) NOT NULL,
+  PRIMARY KEY (`singer`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artist`
+--
+
+LOCK TABLES `artist` WRITE;
+/*!40000 ALTER TABLE `artist` DISABLE KEYS */;
+INSERT INTO `artist` VALUES ('Bob Marley','Reggae'),('Celine Dion','Pop'),('Gabry Ponte','Italo Dance'),('Mariah Carey','R & B');
+/*!40000 ALTER TABLE `artist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoice`
+--
+
+DROP TABLE IF EXISTS `invoice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoice` (
+  `invoiceNum` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` varchar(8) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`invoiceNum`),
+  KEY `fk22_idx` (`userID`),
+  CONSTRAINT `fk22` FOREIGN KEY (`userID`) REFERENCES `user` (`userid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice`
+--
+
+LOCK TABLES `invoice` WRITE;
+/*!40000 ALTER TABLE `invoice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoicelines`
+--
+
+DROP TABLE IF EXISTS `invoicelines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoicelines` (
+  `invoiceNum` int(11) NOT NULL,
+  `trackName` varchar(20) NOT NULL,
+  PRIMARY KEY (`invoiceNum`),
+  CONSTRAINT `fk50` FOREIGN KEY (`invoiceNum`) REFERENCES `invoice` (`invoiceNum`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoicelines`
+--
+
+LOCK TABLES `invoicelines` WRITE;
+/*!40000 ALTER TABLE `invoicelines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoicelines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `track`
+--
+
+DROP TABLE IF EXISTS `track`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `track` (
+  `trackName` varchar(20) NOT NULL,
+  `artist` varchar(40) NOT NULL,
+  `duration` decimal(3,1) NOT NULL,
+  `album` varchar(40) NOT NULL,
+  `price` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`trackName`),
+  KEY `fk1_idx` (`artist`),
+  KEY `fk2_idx` (`album`),
+  CONSTRAINT `fk1` FOREIGN KEY (`artist`) REFERENCES `artist` (`singer`) ON UPDATE CASCADE,
+  CONSTRAINT `fk2` FOREIGN KEY (`album`) REFERENCES `album` (`name`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `track`
+--
+
+LOCK TABLES `track` WRITE;
+/*!40000 ALTER TABLE `track` DISABLE KEYS */;
+INSERT INTO `track` VALUES ('Geordie','Gabry Ponte',3.4,'Geordie',0.49),('My All','Mariah Carey',5.3,'My All',0.99),('My Heart Will Go On','Celine Dion',5.5,'Titanic BSO',0.99),('No Woman No Cry','Bob Marley',4.3,'No Woman No Cry',0.99);
+/*!40000 ALTER TABLE `track` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(8) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `surname` varchar(40) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('1','1','1','1');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-20  9:17:47
